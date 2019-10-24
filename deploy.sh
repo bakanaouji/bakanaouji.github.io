e29d2a2642c5e7681eb1e9378920c4f3c02b3934
bakanaouji <abeken1224@yahoo.co.jp>
2019-10-24 16:18:40 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
-git checkout develop
+#!/bin/bash
+
+echo "hugo"
 hugo
+
+echo "git push to develop branch"
 git add .
 git commit -m $1
 git push origin develop
+
+echo "git checkout master"
 git checkout master
+
+echo "copy files in docs folder"
 \cp -r -f ./docs/ .
 rm -rf ./docs
+
+echo "git push to master branch"
 git add .
 git commit -m $1 
 git push origin master
